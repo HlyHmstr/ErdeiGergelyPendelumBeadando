1ef2de2ec509531cf74d048976fd99f8787d1f13
HlyHmstr <92311453+HlyHmstr@users.noreply.github.com>
2022-01-05 19:54:29 +0100

    Add files via upload

---
+++ PendelumConsoleApp/PendelumConsoleApp/albums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PendelumConsoleApp
+{
+    internal class albums
+    {
+        private string azon;
+        private string szerzonev;
+        private string cim;
+        private string megjel;
+
+        public albums(string sor)
+        {
+            var t = sor.Split(';');
+            azon = t[0];
+            szerzonev = t[1];
+            cim = t[2];
+            megjel = t[3];
+        }
+        public string Azon { get => azon; set => azon = value; }
+        public string Cim { get => cim; set => cim = value; }
+        public string Megjel { get => megjel; set => megjel = value; }
+        public string Szerzonev { get => szerzonev; set => szerzonev = value; }
+
+    }
+}
